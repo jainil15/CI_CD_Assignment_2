d9e7b6ab02d58f725388f22a2522108a85bccf9e
jainil15 <jainilpatel115@gmail.com>
2024-03-10 23:18:38 +0530

    Added different workfllow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world! From Jainil!! ANOTHER !! 222222 !!!!! ");
+  res.send("Hello, world! From Jainil!! ANOTHER !! 222222 !!!!! ON ECS");
 });
 
 const PORT = 3000;
