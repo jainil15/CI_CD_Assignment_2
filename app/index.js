73ec451ed2cb437f2ce2fd08e50c722ad8b956d7
jainil15 <jainilpatel115@gmail.com>
2024-03-11 14:06:06 +0530

    Final testing

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world! From Jainil!! ANOTHER !! 222222 !!!!! ON ECS 2 2 THis is testing asg WOKRING!!!!!!!!!!!!!! SAHI ME CHAL GAYA?");
+  res.send("Hello, world! From Jainil, Sahi me chal gaya???");
 });
 
 const PORT = 3000;
