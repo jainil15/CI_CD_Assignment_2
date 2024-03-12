98f83669c6a2b5ccbdb4041c0998fd240e9b47df
vivekk-promact <vivek.kadam@promactinfo.com>
2024-03-12 15:17:33 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world! From Jainil, Sahi me chal gaya??? WOOO!!! Final Test");
+  res.send("Hello, world! From Jainil, Sahi me chal gaya??? WOOO!!! Final Test, So lets checked by the mentor now");
 });
 
 const PORT = 3000;
